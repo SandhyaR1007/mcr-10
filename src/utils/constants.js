f538a09d2c4f55efd96b600239572f7ecad44cb3
SandhyaR1007 <47480884+SandhyaR1007@users.noreply.github.com>
2023-08-12 20:31:49 +0530

    add: constant for data

---
+++ src/utils/constants.js
@@ -0,0 +1,28 @@
+export const departments = ["Kitchen", "Clothing", "Toy"];
+
+export const columns = [
+  {
+    key: "imageUrl",
+    name: "Image",
+  },
+  {
+    key: "name",
+    name: "Name",
+  },
+  {
+    key: "description",
+    name: "Description",
+  },
+  {
+    key: "price",
+    name: "Price",
+  },
+  {
+    key: "stock",
+    name: "Stock",
+  },
+  {
+    key: "supplier",
+    name: "Supplier",
+  },
+];
