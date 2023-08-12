fa209454e0da0a3307af90bc07a162b7e4ad9435
SandhyaR1007 <47480884+SandhyaR1007@users.noreply.github.com>
2023-08-12 18:49:03 +0530

    add: react-router-dom

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router } from "react-router-dom";
+import App from "./App.jsx";
+import "./index.css";
+import { ProductsProvider } from "./context/ProductsContext.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ProductsProvider>
+      <Router>
+        <App />
+      </Router>
+    </ProductsProvider>
+  </React.StrictMode>
+);
