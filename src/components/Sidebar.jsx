e71855c4de548105e7505623a36215a719e18932
SandhyaR1007 <47480884+SandhyaR1007@users.noreply.github.com>
2023-08-12 18:50:20 +0530

    add: Sidebar with navigation

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,31 @@
+import { NavLink } from "react-router-dom";
+
+const Sidebar = () => {
+  return (
+    <ul className="flex flex-col gap-10 ">
+      <li className="text-gray-400 font-semibold text-lg p-4">
+        <NavLink to="/" className={({ isActive }) => isActive && "text-white"}>
+          Dashboard
+        </NavLink>
+      </li>
+      <li className="text-gray-400 font-semibold text-lg p-4">
+        <NavLink
+          to="/departments"
+          className={({ isActive }) => isActive && "text-white"}
+        >
+          Departments
+        </NavLink>
+      </li>
+      <li className="text-gray-400 font-semibold text-lg p-4">
+        <NavLink
+          to="/products"
+          className={({ isActive }) => isActive && "text-white"}
+        >
+          Products
+        </NavLink>
+      </li>
+    </ul>
+  );
+};
+
+export default Sidebar;
