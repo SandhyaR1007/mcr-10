3f52f937251c2306e54d1669ddecad0234ae241b
SandhyaR1007 <47480884+SandhyaR1007@users.noreply.github.com>
2023-08-12 18:55:24 +0530

    update: added click handler

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,21 @@
-const Card = ({ cardText, cardValue, textColor, link, isDashboard }) => {
+const Card = ({
+  cardText,
+  cardValue,
+  textColor,
+  clickHandler,
+  isDashboard,
+}) => {
   return (
-    <div className="p-5 bg-gray-200 shadow-md rounded-md flex flex-col items-center justify-center">
+    <div
+      className={`p-5 bg-gray-200 shadow-md rounded-md flex flex-col items-center justify-center ${
+        clickHandler && "cursor-pointer"
+      }`}
+      onClick={() => {
+        if (clickHandler) {
+          clickHandler();
+        }
+      }}
+    >
       {isDashboard && (
         <>
           <p className={`${textColor} font-bold text-2xl`}>{cardValue}</p>
