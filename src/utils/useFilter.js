064fc49582711ec5379426f3f21504efd48e5ae6
SandhyaR1007 <47480884+SandhyaR1007@users.noreply.github.com>
2023-08-12 20:31:35 +0530

    add: filter hook

---
+++ src/utils/useFilter.js
@@ -0,0 +1,36 @@
+export const useFilter = () => {
+  const filterByDepartment = (productsData, selectedDepartment) => {
+    return selectedDepartment === "All Departments"
+      ? productsData
+      : productsData.filter(
+          ({ department }) => department === selectedDepartment
+        );
+  };
+  const filterByStock = (productsData, isStock) => {
+    return isStock
+      ? productsData.filter(({ stock }) => stock <= 10)
+      : productsData;
+  };
+  const sortByCategory = (productsData, selectedCategory) => {
+    console.log({ selectedCategory });
+
+    return selectedCategory
+      ? selectedCategory === "name"
+        ? [...productsData].sort((a, b) => {
+            const nameA = a.name.toLowerCase();
+            const nameB = b.name.toLowerCase();
+            if (nameA < nameB) {
+              return -1;
+            }
+            if (nameA > nameB) {
+              return 1;
+            }
+            return 0;
+          })
+        : [...productsData].sort(
+            (a, b) => a[selectedCategory] - b[selectedCategory]
+          )
+      : [...productsData];
+  };
+  return { filterByDepartment, filterByStock, sortByCategory };
+};
