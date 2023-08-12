e430c76c7a4597c3d87cb2b07748c43d18dae042
SandhyaR1007 <47480884+SandhyaR1007@users.noreply.github.com>
2023-08-12 20:32:54 +0530

    update: added logic for filters and filtered data

--- src/context/ProductsContext.jsx
+++ src/context/ProductsContext.jsx
@@ -1,9 +1,11 @@
 import { createContext, useContext, useReducer } from "react";
-import { initialState, productsReducer } from "./productsReducer";
+import { actionTypes, initialState, productsReducer } from "./productsReducer";
+import { useFilter } from "../utils/useFilter";
 
 const ProductsContext = createContext();
 
 export const ProductsProvider = ({ children }) => {
+  const { filterByDepartment, filterByStock, sortByCategory } = useFilter();
   const [state, dispatch] = useReducer(productsReducer, initialState);
   const dashboardAnalytics = state.productsList.reduce(
     (acc, curr) => ({
@@ -14,8 +16,53 @@ export const ProductsProvider = ({ children }) => {
     }),
     { total: 0, delivered: 0, lowStock: 0 }
   );
+  const updateDepartment = (selectedDepartment) => {
+    dispatch({
+      type: actionTypes.FILTER_BY_DEPARTMENT,
+      payload: selectedDepartment,
+    });
+  };
+
+  const updateIsLowStock = (isLowStock) => {
+    dispatch({
+      type: actionTypes.FILTER_BY_STOCK,
+      payload: isLowStock,
+    });
+  };
+
+  const updateSortBy = (selectedCategory) => {
+    dispatch({
+      type: actionTypes.SORT,
+      payload: selectedCategory,
+    });
+  };
+
+  const findProductById = (productId) =>
+    state.productsList.find(({ id }) => Number(id) === Number(productId));
+
+  let filteredProducts = filterByDepartment(
+    state.productsList,
+    state?.filters?.department
+  );
+  filteredProducts = filterByStock(
+    filteredProducts,
+    state?.filters?.isLowStock
+  );
+  filteredProducts = sortByCategory(filteredProducts, state?.filters?.sortBy);
+
   return (
-    <ProductsContext.Provider value={{ dashboardAnalytics }}>
+    <ProductsContext.Provider
+      value={{
+        productsList: state.productsList,
+        filters: state.filters,
+        dashboardAnalytics,
+        filteredProducts,
+        updateDepartment,
+        updateIsLowStock,
+        updateSortBy,
+        findProductById,
+      }}
+    >
       {children}
     </ProductsContext.Provider>
   );
--- src/context/productsReducer.js
+++ src/context/productsReducer.js
@@ -2,10 +2,37 @@ import { inventoryData } from "../data";
 
 export const initialState = {
   productsList: inventoryData,
+  filters: {
+    department: "All Departments",
+    isLowStock: false,
+    sortBy: "",
+  },
+};
+
+export const actionTypes = {
+  FILTER_BY_DEPARTMENT: "FILTER_BY_DEPARTMENT",
+  FILTER_BY_STOCK: "FILTER_BY_STOCK",
+  SORT: "SORT",
+  ADD_NEW_PRODUCT: "ADD_NEW_PRODUCT",
 };
 
 export const productsReducer = (state, action) => {
   switch (action.type) {
+    case actionTypes.FILTER_BY_DEPARTMENT:
+      return {
+        ...state,
+        filters: { ...state.filters, department: action.payload },
+      };
+    case actionTypes.FILTER_BY_STOCK:
+      return {
+        ...state,
+        filters: { ...state.filters, isLowStock: action.payload },
+      };
+    case actionTypes.SORT:
+      return {
+        ...state,
+        filters: { ...state.filters, sortBy: action.payload },
+      };
     default:
       return { ...state };
   }
